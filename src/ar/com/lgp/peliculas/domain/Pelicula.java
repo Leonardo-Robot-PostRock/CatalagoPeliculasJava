a761ab58f44caa89463b564ca9cf53ba9aeaa2de
Leonardo <leonardo38361@gmail.com>
2024-06-17 19:38:32 -0300

    se crea clase de dominio

---
+++ src/ar/com/lgp/peliculas/domain/Pelicula.java
@@ -0,0 +1,15 @@
+package ar.com.lgp.peliculas.domain;
+
+public class Pelicula {
+	private String nombre;
+	
+	public Pelicula() {
+		
+	}
+
+	public Pelicula(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
